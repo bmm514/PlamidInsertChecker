113c46b917820d06dc64bf332c8926897a62b550
Ben Murcott <bmm41@bath.ac.uk>
2023-12-08 17:06:10 +0000

    Cleaning up __main__.py

--- plasmidin/__main__.py
+++ plasmidin/__main__.py
@@ -1,10 +1,9 @@
+from Bio.Seq import Seq
+from Bio.Restriction import RestrictionBatch, AllEnzymes, Analysis, CommOnly
 
 from plasmidin import cut_and_insert, RSFinder, RSInserter
 
 def test_RSFinder():
-    from Bio.Seq import Seq
-    from Bio.Restriction import RestrictionBatch, AllEnzymes
-
     input_seq = Seq('ATTTTCTGAATTCGCTAACGTTA')
     dna_seq = Seq('AAAAGAATTCNNNNNNAACGTTTAT')
 
@@ -38,9 +37,6 @@ def test_RSFinder():
 
     # print(rsfinder.supplier_names)
 def test_RSInserter():
-    from Bio.Seq import Seq
-    from Bio.Restriction import RestrictionBatch, AllEnzymes
-
     backbone_seq = Seq('ATTTTCTGAATTCGCTAACGTTA')
     backbone_linear = False
     insert_seq = Seq('AAAAGAATTCNNNNNNAACGTTTAT')
@@ -55,30 +51,28 @@ def test_RSInserter():
 
 
 
-def test_main():
-    from Bio.Seq import Seq
-    from Bio.Restriction import Analysis, RestrictionBatch, CommOnly
-    plasmid_seq = Seq('ATTTTCTGAATTCGCTAACGTTA')
-    dna_seq = Seq('AAAAGAATTCNNNNNNAACGTTTAT')
+# def test_main():
+#     plasmid_seq = Seq('ATTTTCTGAATTCGCTAACGTTA')
+#     dna_seq = Seq('AAAAGAATTCNNNNNNAACGTTTAT')
 
-    (analysis_plasmid, analysis_dna), (filter_plasmid_rs, filter_dna_rs), shared_enzymes = filter_seqs(plasmid_seq, False, dna_seq, True)
+#     # (analysis_plasmid, analysis_dna), (filter_plasmid_rs, filter_dna_rs), shared_enzymes = filter_seqs(plasmid_seq, False, dna_seq, True)
     
-    inserted_seq = cut_and_insert(
-        plasmid_seq, filter_plasmid_rs, ('EcoRI', 'AclI'),
-        dna_seq, filter_dna_rs, ('EcoRI', 'AclI')
-        )
+#     inserted_seq = cut_and_insert(
+#         plasmid_seq, filter_plasmid_rs, ('EcoRI', 'AclI'),
+#         dna_seq, filter_dna_rs, ('EcoRI', 'AclI')
+#         )
 
-    csvout_plasmid = '/home/bmm41/PhD_VH/SWbioDTP_taught/DataSciMachLearn/plasmid_info.csv'
-    csvout_dna = '/home/bmm41/PhD_VH/SWbioDTP_taught/DataSciMachLearn/dna_info.csv'
-    csvout_inserted = '/home/bmm41/PhD_VH/SWbioDTP_taught/DataSciMachLearn/inserted_info.csv'
+#     csvout_plasmid = '/home/bmm41/PhD_VH/SWbioDTP_taught/DataSciMachLearn/plasmid_info.csv'
+#     csvout_dna = '/home/bmm41/PhD_VH/SWbioDTP_taught/DataSciMachLearn/dna_info.csv'
+#     csvout_inserted = '/home/bmm41/PhD_VH/SWbioDTP_taught/DataSciMachLearn/inserted_info.csv'
 
-    # make_restriction_enzyme_table(analysis_plasmid, csvout_plasmid, shared_enzymes)
-    # make_restriction_enzyme_table(analysis_dna, csvout_dna, shared_enzymes)
+#     # make_restriction_enzyme_table(analysis_plasmid, csvout_plasmid, shared_enzymes)
+#     # make_restriction_enzyme_table(analysis_dna, csvout_dna, shared_enzymes)
 
-    analysis_inserted = Analysis(RestrictionBatch(CommOnly), inserted_seq, True)
-    # make_restriction_enzyme_table(analysis_inserted, csvout_inserted, {})
+#     analysis_inserted = Analysis(RestrictionBatch(CommOnly), inserted_seq, True)
+#     # make_restriction_enzyme_table(analysis_inserted, csvout_inserted, {})
 
-    return analysis_plasmid, analysis_dna
+#     return analysis_plasmid, analysis_dna
 
 if __name__ == '__main__':
     # test_main()
